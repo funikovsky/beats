5a2661c8b91aa12499005ffa6133677db3112298
funikovsky <88435819+funikovsky@users.noreply.github.com>
2021-10-04 19:26:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,24 +2,29 @@ const burger = document.querySelector('.humburger');
 const fullMenu = document.querySelector('.fullscreen-menu');
 const close = document.querySelector('.fullscreen-menu__close');
 const body = document.querySelector('body');
+const active = document.querySelector('active')
+const menuLink = document.querySelectorAll('.menu--vertical .menu__link');
+
+
 
 
 burger.addEventListener("click", e => {
     e.preventDefault();
     fullMenu.classList.add('active');
-    
-    
-    
 });
-
-
 
 close.addEventListener("click", e => {
     e.preventDefault();
     fullMenu.classList.remove('active');
     
-    
-    
 });
+
+menuLink.forEach(item => {
+    item.addEventListener('click', e => {
+        fullMenu.classList.remove('active');
+    })
+})
+
+
 
 
