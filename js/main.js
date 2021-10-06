3ee8c1aceba25323480df29dfd23347fa06a321a
funikovsky <88435819+funikovsky@users.noreply.github.com>
2021-10-06 16:56:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ const close = document.querySelector('.fullscreen-menu__close');
 const body = document.querySelector('body');
 const active = document.querySelector('active')
 const menuLink = document.querySelectorAll('.menu--vertical .menu__link');
+const memberInfo = document.querySelectorAll('.member__info')
+const memberDecor = document.querySelectorAll('.member__info-decor-symbol')
+const memberDesc = document.querySelectorAll('.member__desc')
+const memberDescActive = document.querySelectorAll('.member__desc--active')
+
+
 
 
 
@@ -25,6 +31,34 @@ menuLink.forEach(item => {
         fullMenu.classList.remove('active');
     })
 })
+
+//модальное окно секции КОМАНДА
+
+for(let item of memberInfo) {
+    item.addEventListener('click', e => {
+        const infoDecor = item.lastElementChild.children[0];
+        const descActive = item.parentElement.lastElementChild
+        const descHeight = getComputedStyle(descActive).height;
+        const currentHeight = parseInt(descHeight);
+        
+        if(currentHeight == 0) {
+            infoDecor.classList.add('decor--active');
+            descActive.classList.add('member__desc--active');
+        
+        } else  {
+            descActive.classList.remove('member__desc--active');
+            infoDecor.classList.remove('decor--active');
+                 
+            }
+        
+    })
+
+}
+
+ 
+    
+
+
 
 
 
